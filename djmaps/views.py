106f677d0af7a99001e9f8b7322990687cf35225
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2019-01-24 16:20:47 -0600

    Update views.py

--- djmaps/views.py
+++ djmaps/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def default_map(request):
-	return render(request, 'default.html', {'mapbox_access_token': 'pk.eyJ1IjoiZ2VhdXhzaGVhdXgiLCJhIjoiY2pxcjI5MW5oMGlsYzQycWptMThqbm81cSJ9.MGGgmGzgWbUXqkTWjvK8uQ'})
+	return render(request, 'default.html', {'mapbox_access_token': 'token'})
